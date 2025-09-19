69be33f68754e10a55a7d56c07c3855cc9fdfbc0
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-09-20 01:39:47 +0300

    refactor: update Event model

--- api/app/models/event.rb
+++ api/app/models/event.rb
@@ -1,38 +1,45 @@
 class Event < ApplicationRecord
-  belongs_to :organizer, class_name: "User", optional: true
+  belongs_to :organizer, class_name: "User", optional: false
 
   enum status: {
-    published: "PUBLISHED",
-    draft: "DRAFT",
-    cancelled: "CANCELLED",
-    completed: "COMPLETED",
-    in_review: "IN_REVIEW",
-    rejected: "REJECTED",
-    archives: "ARCHIVED"
-  }
+  draft: 0,
+  published: 1,
+  in_review: 2,
+  rejected: 3,
+  cancelled: 4,
+  completed: 5,
+  archived: 6
+}
 
+  # Validations for text fields
   validates :title, presence: true, length: { maximum: 150 }
   validates :description, length: { maximum: 500 }, allow_blank: true
-  validates :location, presence: true
-  validates :start_date, presence: true
-  validates :finish_date, presence: true
-  validate  :start_date_in_future
-  validate  :finish_date_after_start_date
-  validates :ticket_price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+  # Validations for required dates and location
+  validates :location, :start_date, :finish_date, presence: true
+
+  # Custom validation methods
+  validates :validate_start_date
+  validates :validate_finish_date
+
+  # Validations for numeric fields
+  validates :ticket_price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: false
 
   private
 
-  def start_date_in_future
+  def validate_start_date
     return if start_date.blank?
-    if start_date < Time.current
-      errors.add(:start_date, "must be in the future")
+
+    if start_date < Time.current.utc
+      errors.add(:start_date, :not_in_future)
     end
   end
 
-  def finish_date_after_start_date
+  def validate_finish_date
     return if start_date.blank? || finish_date.blank?
+
     if finish_date <= start_date
-      errors.add(:finish_date, "must be after start date")
+      errors.add(:finish_date, :after_start_date)
     end
   end
 end
