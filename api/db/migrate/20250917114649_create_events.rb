4dfcee446c3a861b5c328b9ddc1538a96ae8b9ca
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-09-20 01:40:46 +0300

    refactor: update CreateEvents migration

--- api/db/migrate/20250917114649_create_events.rb
+++ api/db/migrate/20250917114649_create_events.rb
@@ -4,17 +4,22 @@ class CreateEvents < ActiveRecord::Migration[8.0]
       t.string  :title, null: false
       t.text    :description
       t.string  :location, null: false
+      t.st_point :coordinates, geographic: true
       t.datetime :start_date, null: false
       t.datetime :finish_date
       t.integer :participant_capacity
-      t.decimal :ticket_price, precision: 10, scale: 2
-      t.string  :status, default: "draft"
-      t.references :organizer, type: :uuid, foreign_key: { to_table: :users }, null: true
+      t.decimal :ticket_price, precision: 10, scale: 2, default: 0.0
+      t.check_constraint "ticket_price >= 0", name: "ticket_price_non_negative"
+      t.integer :status, default: 0
+      t.references :organizer, type: :uuid, foreign_key: { to_table: :users }, null: false
 
       t.timestamps
     end
 
     add_index :events, :start_date
-    add_index :events, :location
+    add_index :events, :coordinates, using: :gist
+
+    # add_index :events, [:location, :start_date]
+    # Good for precise city searches. If we plan on partial names, the index is not helpful.
   end
 end
