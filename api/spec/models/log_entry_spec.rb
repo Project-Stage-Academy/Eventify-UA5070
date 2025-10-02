d871bfa181ee49ffc23c60d0ff18f431f590e12a
SomeTyraredGuy <iuriich2@gmail.com>
2025-10-02 21:08:01 +0000

    test: improve specs for LogEntry and LogEntryCreationJob

--- api/spec/models/log_entry_spec.rb
+++ api/spec/models/log_entry_spec.rb
@@ -1,11 +1,20 @@
 require "rails_helper"
 
 RSpec.describe LogEntry, type: :model do
+  let(:log_entry) { described_class.new(user_id: 1, event_id: 1, action: :event_member_created) }
+
   describe "validations" do
-    subject { described_class.new(user_id: 1, event_id: 1, action: :event_member_created) }
+    subject { log_entry }
 
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:event_id) }
     it { should validate_presence_of(:action) }
+  end
+
+  describe "actions enum (MongoidEnum)" do
+    it "validates inclusion in defined actions" do
+      log_entry.action = :undefined_action
+      expect(log_entry).not_to be_valid
+    end
   end
 end
