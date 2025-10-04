d6f983814504c930db3a0cfbb1d8d23d1925de65
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-10-04 16:17:47 +0200

    fix: correct lint issues in CreateEventOrganizer migration

--- api/db/migrate/20251004133351_create_event_organizers.rb
+++ api/db/migrate/20251004133351_create_event_organizers.rb
@@ -7,6 +7,6 @@ class CreateEventOrganizers < ActiveRecord::Migration[8.0]
       t.timestamps
     end
 
-    add_index :event_organizers, [:event_id, :user_id], unique: true
+    add_index :event_organizers, [ :event_id, :user_id ], unique: true
   end
 end
