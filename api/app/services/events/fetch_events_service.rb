079dcc5ffb5467d54e30d6619f7315eaa997e7d3
romanpelekh136 <rpeleh19@gmail.com>
2025-10-04 17:51:44 +0000

    feat: separated pagination and sorting functionality from events service to concerns

--- api/app/services/events/fetch_events_service.rb
+++ api/app/services/events/fetch_events_service.rb
@@ -1,4 +1,7 @@
 class Events::FetchEventsService
+  include Sortable
+  include Paginatable
+
   SORTABLE_COLUMNS = {
     "title" => :title,
     "start_date" => :start_date,
@@ -11,21 +14,7 @@ class Events::FetchEventsService
 
   def call
     events = Event.all
-    events = sort_events(events)
-    paginate_events(events)
-  end
-
-  private
-
-  def sort_events(events)
-    sort_column = SORTABLE_COLUMNS[@params[:sort].to_s] || :id
-    sort_direction = %w[asc desc].include?(@params[:direction]) ? @params[:direction].to_sym : :asc
-    events.order(sort_column => sort_direction)
-  end
-
-  def paginate_events(events)
-    per_page = [ @params[:per_page].to_i, 50 ].min
-    per_page = 10 if per_page <= 0
-    events.page(@params[:page]).per(per_page)
+    events = sort(events, @params, SORTABLE_COLUMNS)
+    paginate(events, @params)
   end
 end
