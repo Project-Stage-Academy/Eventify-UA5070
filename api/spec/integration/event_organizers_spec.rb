31c4db8e51bebe5a433d0eb466fd0ceece5164b3
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-10-06 20:02:14 +0200

    feat: add Swagger/OpenAPI docs for EventOrganizers API

---
+++ api/spec/integration/event_organizers_spec.rb
@@ -0,0 +1,87 @@
+require 'swagger_helper'
+
+RSpec.describe 'EventOrganizers API', type: :request do
+  path '/api/v1/events/{event_id}/organizers' do
+    post 'Add an organizer to an event' do
+      tags 'EventOrganizers'
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :Authorization, in: :header, type: :string, description: 'Bearer token'
+      parameter name: :event_id, in: :path, type: :integer
+      parameter name: :body, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer }
+        },
+        required: [ 'user_id' ]
+      }
+
+      let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
+      let(:event) { create(:event, organizer_user: user) }
+      let(:event_id) { event.id }
+      let(:Authorization) { "Bearer #{JwtService.issue_tokens_for(user)[:access_token]}" }
+      let(:body) { { user_id: another_user.id } }
+
+      response '201', 'organizer added' do
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['id']).to eq(another_user.id)
+          expect(event.event_organizers.pluck(:user_id)).to include(another_user.id)
+        end
+      end
+
+      response '403', 'forbidden' do
+        let(:Authorization) { "Bearer #{JwtService.issue_tokens_for(another_user)[:access_token]}" }
+        run_test!
+      end
+
+      response '422', 'unprocessable entity' do
+        before do
+          create(:event_organizer, event: event, user: another_user)
+        end
+        run_test! do |response|
+          expect(JSON.parse(response.body)['errors']).to be_present
+        end
+      end
+    end
+  end
+
+  path '/api/v1/events/{event_id}/organizers/{user_id}' do
+    delete 'Remove an organizer from an event' do
+      tags 'EventOrganizers'
+      produces 'application/json'
+
+      parameter name: :Authorization, in: :header, type: :string, description: 'Bearer token'
+      parameter name: :event_id, in: :path, type: :integer
+      parameter name: :user_id, in: :path, type: :integer
+
+      let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
+      let(:event) { create(:event, organizer_user: user) }
+      let(:event_id) { event.id }
+      let(:Authorization) { "Bearer #{JwtService.issue_tokens_for(user)[:access_token]}" }
+
+      response '200', 'organizer removed' do
+        let(:user_id) { another_user.id }
+        before do
+          create(:event_organizer, event: event, user: another_user)
+        end
+        run_test! do |response|
+          expect(event.event_organizers.where(user_id: another_user.id)).to be_empty
+        end
+      end
+
+      response '403', 'cannot remove last organizer or unauthorized' do
+        let(:user_id) { user.id }
+        run_test!
+      end
+
+      response '404', 'organizer not found' do
+        let(:user_id) { 999 }
+        run_test!
+      end
+    end
+  end
+end
--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -1,10 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+ENV["JWT_SECRET_KEY"] ||= "test_secret_key"
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'pundit/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -62,6 +64,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
