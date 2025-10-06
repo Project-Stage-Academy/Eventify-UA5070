67fa289b3ab8d02a5e3bf023d9341a04b9ab4567
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-10-06 19:59:05 +0200

    test: add FactoryBot factories for Event and EventOrganizer and User

---
+++ api/spec/factories/events.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :event do
+    title { "Test Event" }
+    location { "Kyiv" }
+    start_date { 1.day.from_now }
+    finish_date { 2.days.from_now }
+    participant_capacity { 10 }
+    ticket_price { 100 }
+
+    transient do
+      organizer_user { nil } 
+    end
+
+    after(:create) do |event, evaluator|
+      if evaluator.organizer_user
+        FactoryBot.create(:event_organizer, event: event, user: evaluator.organizer_user, is_primary: true)
+      else
+        FactoryBot.create(:event_organizer, event: event, user: FactoryBot.create(:user), is_primary: true)
+      end
+    end
+  end
+end
