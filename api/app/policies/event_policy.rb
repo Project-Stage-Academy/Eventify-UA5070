81af57d05cf2a60e914122f313b43b32e7cad7cc
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-10-06 19:40:25 +0200

    feat: add ApplicationPolicy and update EventPolicy for organizer authorization

--- api/app/policies/event_policy.rb
+++ api/app/policies/event_policy.rb
@@ -1,23 +1,13 @@
 class EventPolicy < ApplicationPolicy
   def update?
-    user.admin? || record.users.include?(user)
+    user.has_role?(:admin) || record.organizers.include?(user)
   end
 
   def destroy?
-    user.admin? || record.event_organizers.find_by(user: user)&.is_primary?
+  user.has_role?(:admin) || !!record.event_organizers.find_by(user: user)&.is_primary?
   end
 
   def manage_organizers?
-    user.admin? || record.event_organizers.find_by(user: user)&.is_primary?
-  end
-
-  class Scope < ApplicationPolicy::Scope
-    def resolve
-      if user.admin?
-        scope.all
-      else
-        scope.joins(:event_organizers).where(event_organizers: { user_id: user.id }).distinct
-      end
-    end
+    user.has_role?(:admin) || !!record.event_organizers.find_by(user: user)&.is_primary?
   end
 end
