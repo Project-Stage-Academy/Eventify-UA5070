f80e711822b22510fb0660b43ff9c6abbf5bfe79
SomeTyraredGuy <iuriich2@gmail.com>
2025-10-08 07:07:33 +0000

    feat: EventMember model

--- api/app/models/event_member.rb
+++ api/app/models/event_member.rb
@@ -1,2 +1,10 @@
 class EventMember < ApplicationRecord
+  belongs_to :event
+  belongs_to :member
+
+  validates :user, presence: true
+  validates :event, presence: true
+  validates :ticket_qr_code, presence: true, uniqueness: true, length: { minimum: 10, maximum: 10 }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_nil: true
+  validates :comment, length: { maximum: 1000 }, allow_blank: true
 end
