a58646090e461a0f6ed76a5f89609c439de337c3
a2teak <artur.raievskyi@gmail.com>
2025-10-15 14:35:00 +0000

    fix: updated password to be not hardcoded

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.unique.email }
-    password { "secret123" }
+    password { Faker::Internet.password }
   end
 end
