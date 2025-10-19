6afa7c27482e552f710307e31a972bd9248d6bf0
a2teak <artur.raievskyi@gmail.com>
2025-10-19 13:59:21 +0000

    swagger port via env variable

--- api/config/initializers/rswag_api.rb
+++ api/config/initializers/rswag_api.rb
@@ -1,6 +1,7 @@
 Rswag::Api.configure do |c|
   c.openapi_root = File.join(Rails.root, "swagger")
   c.swagger_filter = lambda do |swagger, env|
-    swagger["servers"] = [ { "url" => "http://localhost:3000" } ]
+    base = ENV["SWAGGER_SERVER_URL"].presence || "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}"
+    swagger["servers"] = [ { "url" => base } ]
   end
 end
