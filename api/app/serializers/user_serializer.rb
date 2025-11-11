ba2c46b3eb60589664da3e0e209de4733efd268c
a2teak <artur.raievskyi@gmail.com>
2025-11-11 12:24:44 +0000

    redundant comments removal

--- api/app/serializers/user_serializer.rb
+++ api/app/serializers/user_serializer.rb
@@ -1,17 +1,3 @@
-# class UserSerializer
-#   def initialize(user)
-#     @user = user
-#   end
-#   я
-#   def as_json(*)
-#     {
-#       id: @user.id,
-#       name: @user.name,
-#       email: @user.email,
-#       roles: @user.roles.select(:id, :name).map { |role| { id: role.id, name: role.name } }
-#     }
-#   end
-# end
 class UserSerializer < Blueprinter::Base
   identifier :id
   fields :name, :email
