97c3c9ad1024275dd39022ad8d9f706e67816df5
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-11-12 21:20:19 +0100

    chore: apply RuboCop auto-corrections

--- api/app/services/event_service.rb
+++ api/app/services/event_service.rb
@@ -40,10 +40,10 @@ class EventService
     Result.new(success: false, event: nil, errors: e.record.errors.full_messages.uniq)
   rescue ActiveRecord::RecordNotUnique => e
     Rails.logger.error("Record not unique: #{e.message}\n#{e.backtrace.join("\n")}")
-    Result.new(success: false, event: nil, errors: ["A record with these details already exists."])
+    Result.new(success: false, event: nil, errors: [ "A record with these details already exists." ])
   rescue ActiveRecord::StatementInvalid => e
     Rails.logger.error("Database statement invalid: #{e.message}\n#{e.backtrace.join("\n")}")
-    Result.new(success: false, event: nil, errors: ["There was a problem saving the event. Please check your input and try again."])
+    Result.new(success: false, event: nil, errors: [ "There was a problem saving the event. Please check your input and try again." ])
   end
 
   def update(event)
